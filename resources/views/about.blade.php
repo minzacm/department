a8ddef98e9c3782e22936e1553b0337703c77c11
minza <minzacm@gmail.com>
2021-05-30 19:26:13 +0530

    done

---
+++ resources/views/about.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Student</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/contact">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/gallery">Gallery</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/about">About</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<div class="container">
+<div class="row">
+<div class="col col-12 col-sm-6 col-mid-6 col-lg-6 col-xl-6 col-xxl-6">
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore perspiciatis magnam fuga quas qui aspernatur voluptatum sit impedit aliquid incidunt cupiditate, eligendi non nihil illo iusto placeat nobis quibusdam mollitia.</div>
+
+<div class="col col-12 col-sm-6 col-mid-6 col-lg-6 col-xl-6 col-xxl-6">
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas nostrum dolor perspiciatis asperiores, reiciendis, sed tempore atque quidem deleniti, quisquam error? Dolorum accusantium aperiam impedit quae exercitationem numquam ducimus officia.</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+   
+</body>
+</html>
+
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Student</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/contact">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/gallery">Gallery</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/about">About</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<div class="conatiner">
+<div class="row">
+<div class="col col-12 col-sm-4 col-mid-4 col-lg-4 col-xl-4 col-xxl-4"></div>
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://collegelearners.com/wp-content/uploads/2018/07/universities-in-america.jpg" class="d-block w-100"  width="600px" height="600px" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>WELCOME</h5>
+        <p>View your Departments</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="https://www.timeshighereducation.com/sites/default/files/styles/the_breaking_news_image_style/public/harvard_0_0.jpg?itok=KN1GjFZx" class="d-block w-100" width="600px" height="600px" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Here you can see what you are looking for</h5>
+        
+      </div>
+    </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+<div class="col col-12 col-sm-4 col-mid-4 col-lg-4 col-xl-4 col-xxl-4">
+<table class="table table-borderless">
+<tr>
+    <td>Name</td>
+    <td><input type="text" class="form-control"></td>
+</tr>
+<tr>
+    <td>Department</td>
+    <td><input type="text" class="form-control"></td>
+</tr>
+<tr>
+    <td>Department Number</td>
+    <td><input type="text" class="form-control"></td>
+</tr>
+<tr>
+    <td></td>
+    <td><button class="btn btn-success">SUBMIT</button></td>
+</tr>
+</table>
+
+</div>
+<div class="col col-12 col-sm-4 col-mid-4 col-lg-4 col-xl-4 col-xxl-4">
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
+</body>
+</html>
